2788b1c73476dccd8a0051894618d288fb5b1205
Brandon Menc <brandonmenc@icloud.com>
2019-03-25 17:12:26 -0500

    mix new

---
+++ lib/nerves_mount_manager.ex
@@ -0,0 +1,18 @@
+defmodule NervesMountManager do
+  @moduledoc """
+  Documentation for NervesMountManager.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesMountManager.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_mount_manager_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesMountManagerTest do
+  use ExUnit.Case
+  doctest NervesMountManager
+
+  test "greets the world" do
+    assert NervesMountManager.hello() == :world
+  end
+end
